100acc5bdb08e3558eaeb32614b1af05a841c244
tradewithmadhan <tradewithmadhan@outlook.com>
2026-08-21 07:46:14 +0530

    chore: remove frontend/dist from tracking (rebuilt by CI/CD)

--- frontend/dist/assets/table-BiGs3NTr.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-CxEvg8o7.js";var t=e(`table`,[[`path`,{d:`M12 3v18`,key:`108xh3`}],[`rect`,{width:`18`,height:`18`,x:`3`,y:`3`,rx:`2`,key:`afitv7`}],[`path`,{d:`M3 9h18`,key:`1pudct`}],[`path`,{d:`M3 15h18`,key:`5xshup`}]]);export{t};
